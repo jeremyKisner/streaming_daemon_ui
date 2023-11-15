20095c191bf2ce20e19996d572a3bbf7741c56e9
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-11-14 21:31:00 -0500

    changed from timer to checking the stream

--- lib/main.dart
+++ lib/main.dart
@@ -26,28 +26,55 @@ class MusicPlayer extends StatefulWidget {
 
 class _MusicPlayerState extends State<MusicPlayer> {
   final _player = AudioPlayer();
-  Timer? _timer;
+  StreamSubscription<Duration?>? _positionSubscription;
+  StreamSubscription<Duration?>? _durationSubscription;
+  Duration? _duration;
+  Duration? _position;
+  bool _isLoading = false;
 
   @override
   void dispose() {
     _player.dispose();
-    _timer?.cancel();
+    _positionSubscription?.cancel();
+    _durationSubscription?.cancel();
     super.dispose();
   }
 
   void playMusic() async {
+    print('playMusic called');
+    _isLoading = true;
     var url = 'http://localhost:8080/beepstream';
     await _player.setUrl(url);
     _player.play();
-    _timer = Timer.periodic(Duration(seconds: 5), (timer) async {
-      await _player.setUrl(url);
-      _player.play();
+    _isLoading = false;
+
+    _positionSubscription = _player.positionStream.listen((position) {
+      _position = position;
+      _checkAudioProgress();
+    });
+
+    _durationSubscription = _player.durationStream.listen((duration) {
+      _duration = duration;
+      _checkAudioProgress();
     });
   }
 
+  void _checkAudioProgress() async {
+    if (_position != null && _duration != null) {
+      double progress = _position!.inMilliseconds / _duration!.inMilliseconds;
+      if (progress >= 0.75) {
+        // If the audio is 75% complete and the player is not already loading the next audio
+        if (!_isLoading) {
+          playMusic(); // Request more data here
+        }
+      }
+    }
+  }
+
   void stopMusic() {
-    _timer?.cancel(); // Cancel the timer first
     _player.stop();
+    _positionSubscription?.cancel();
+    _durationSubscription?.cancel();
   }
 
   @override
